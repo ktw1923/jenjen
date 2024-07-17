2b54b1bd1283df9d31a1d5df521ced76dbea668a
TaeWoongKim <jason1923@naver.com>
2024-07-17 10:50:26 +0900

    240717 test

---
+++ src/main/java/org/example/simple1/controller/TestController.java
@@ -0,0 +1,34 @@
+package org.example.simple1.controller;
+
+import org.example.simple1.dto.MemberDto;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class TestController {
+    @GetMapping("/")
+    public MemberDto test() {
+        MemberDto memberDto = new MemberDto();
+        memberDto.setName("test");
+        memberDto.setAge(20);
+        return memberDto;
+    }
+
+    @GetMapping("/test2")
+    public MemberDto test2() {
+        MemberDto memberDto = new MemberDto();
+        memberDto.setName("test");
+        memberDto.setAge(20);
+        return memberDto;
+    }
+
+    @GetMapping("/test3")
+    public MemberDto test3() {
+        MemberDto memberDto = new MemberDto();
+        memberDto.setName("test3");
+        memberDto.setAge(30);
+        return memberDto;
+    }
+}
